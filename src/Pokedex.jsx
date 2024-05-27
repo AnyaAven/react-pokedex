67f55642c9cf60708c64b750d39d4a0d8db2f064
Anya Aven <leafeon96@gmail.com>
2024-05-27 19:27:38 -0400

    Finished

--- src/Pokedex.jsx
+++ src/Pokedex.jsx
@@ -1,9 +1,17 @@
 import Pokecard from "./Pokecard.jsx";
 
+/** Pokedex div of a collection of pokemon cards */
 function Pokedex({ pokemon }) {
 
-  return pokemon.map(
-    p => <Pokecard name={p.name} type={p.type} exp={p.base_experience} />);
+  const pokecards = pokemon.map(
+    p => <Pokecard
+      name={p.name}
+      type={p.type}
+      exp={p.base_experience}
+      id={p.id} />
+  );
+
+  return (<div className="Pokedex">{pokecards}</div>);
 }
 
 export default Pokedex;
