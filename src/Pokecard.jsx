f0c315b796fc415530fdf470f42fc3f84ef262ce
Anya Aven <leafeon96@gmail.com>
2024-05-27 19:27:16 -0400

    Finished Pokecard

--- src/Pokecard.jsx
+++ src/Pokecard.jsx
@@ -1,1 +1,15 @@
-// where the div for the card is made
+/** Pokecard div of name, img, type, exp */
+function Pokecard({ name, type, exp, id }) {
+
+  const imgUrl =
+  `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`
+
+  return (<div className="Pokecard">
+    <h1 className="Pokecard-name">{name}</h1>
+    <img src={imgUrl} className="Pokecard-img"></img>
+    <p className="Pokecard-type">Type: {type}</p>
+    <p className="Pokecard-exp">EXP: {exp}</p>
+  </div>);
+}
+
+export default Pokecard;
