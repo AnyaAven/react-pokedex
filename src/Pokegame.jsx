c1bfc6819daecfbfaf87bfde56dc2fa79f33e9e0
Anya Aven <leafeon96@gmail.com>
2024-05-27 20:39:05 -0400

    WIP

---
+++ src/Pokegame.jsx
@@ -0,0 +1,26 @@
+import { sampleSize } from "lodash";
+import Pokedex from "./Pokedex.jsx";
+
+const POKEMON = [
+  { id: 4, name: 'Charmander', type: 'fire', base_experience: 62 },
+  { id: 7, name: 'Squirtle', type: 'water', base_experience: 63 },
+  { id: 11, name: 'Metapod', type: 'bug', base_experience: 72 },
+  { id: 12, name: 'Butterfree', type: 'flying', base_experience: 178 },
+  { id: 25, name: 'Pikachu', type: 'electric', base_experience: 112 },
+  { id: 39, name: 'Jigglypuff', type: 'normal', base_experience: 95 },
+  { id: 94, name: 'Gengar', type: 'poison', base_experience: 225 },
+  { id: 133, name: 'Eevee', type: 'normal', base_experience: 65 }
+];
+
+/** Pokegame div that has two pokedices */
+function Pokegame({pokemon = POKEMON}) {
+  const totalExp = pokemon.reduce((acc, p) => (acc += p.base_experience), 0);
+
+
+  return (<div className="Pokegame">
+    <Pokedex pokemon={POKEMON} />
+    <Pokedex pokemon={POKEMON} />
+  </div>);
+}
+
+export default Pokegame;
